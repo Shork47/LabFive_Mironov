ea5159613c5c68228d930b50cfff3533eaeea506
Shork47 <mironovdima67@gmail.com>
2025-04-20 18:49:32 +0500

    Initial commit

---
+++ app/src/main/java/com/example/lab5mironovdm609_41m/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.lab5mironovdm609_41m
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        val tvHello = findViewById<TextView>(R.id.tvHello)
+        val btnHello = findViewById<Button>(R.id.btnHello)
+
+        btnHello.setOnClickListener{
+            tvHello.text = getString(R.string.hello)
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lab5 Mironov D.M. 609-41m"
+include(":app")
+ 
